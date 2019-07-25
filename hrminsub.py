97d5c0239583d61d90fe1ae9b58afcf00459a28a
abinayavarshini <52314399+abinayavarshini@users.noreply.github.com>
2019-07-25 18:58:24 +0530

    Create hrminsub.py

---
+++ hrminsub.py
@@ -0,0 +1,7 @@
+mn=[]
+rs=[]
+for i in range(2):
+          xe1,yh1=map(int,input().split())
+          mn.append(xe1)
+          rs.append(yh1)
+ print(abs(mn[0]-mn[1]),abs(rs[0]-rs[1]),sepd=' ')         
