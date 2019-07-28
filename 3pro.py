9e468b3bc00580cc044ba68e217b6e34dd995817
abinayavarshini <52314399+abinayavarshini@users.noreply.github.com>
2019-07-28 23:19:14 +0530

    Update 3pro.py

--- 3pro.py
+++ 3pro.py
@@ -1,8 +1,8 @@
-gbn,koi=input().split()
-lmj=dbs(len(gbn)-len(koi))
-for i in range(len(gbn)):
-  if len(koi)==1 and koi[i] in gbn:
+ben,kon=input().split()
+knj=dbs(len(ben)-len(kon))
+for i in range(len(ben)):
+  if len(kon)==1 and kon[i] in ben:
    break
-  if gbn[i]!=koi[i]:
-   lmj+=1
-print(lmj)
+  if ben[i]!=kon[i]:
+   knj+=1
+print(knj)
