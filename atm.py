64d040547c55995c8ac53d4d46156ddc0455dc3d
abinayavarshini <52314399+abinayavarshini@users.noreply.github.com>
2019-07-31 15:47:53 +0530

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,20 @@
+username = input("Please enter your username : ")
+pin = input("Please enter your pin : ")
+balance=5000
+data=["abc","123"]
+if (username and pin in data):
+  print("Welcome")
+  print("1.current account\n 2.saving account")
+else:
+  print (" Sorry username and pin invalid ")
+c=int(input())
+if(c==1):
+     print("1.withdraw\n 2.deposit\n 3.balance enquiry")
+elif (c==2):
+     print("1.withdraw\n 2.deposit\n 3.balance enquiry")
+else:
+     print("invalid select 1 or 2")
+n=int(input())
+if( n==1)
+if amt<5000
+print("not sufficint")
