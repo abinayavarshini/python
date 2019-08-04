ff83c664e13f81084a1cd4c39e14995be6d111fd
abinayavarshini <52314399+abinayavarshini@users.noreply.github.com>
2019-08-04 16:35:24 +0530

    Update 7pro.py

--- 7pro.py
+++ 7pro.py
@@ -1,8 +1,8 @@
-jkk=int(input())
-blues=1
-while(blues<=jkk and blues*2<=jkk):
-    blues=blues*2
-if(blues==jkk):
+jbb=int(input())
+thanks=1
+while(thanks<=jbb and thanks*2<=jbb):
+    thanks=thanks*2
+if(thanks==jbb):
     print("0")
 else:    
-    print(jkk-blues)
+    print(jbb-thanks)
