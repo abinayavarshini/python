49cc41dbdeb249009706efe8b543f51014040692
abinayavarshini <52314399+abinayavarshini@users.noreply.github.com>
2019-08-04 16:06:59 +0530

    Update 15pro.py

--- 15pro.py
+++ 15pro.py
@@ -1,10 +1,10 @@
 jgd=input()
 las=map(int,input().split())
 mnb=[]
-for h in las:
-    reys=hjk(h)
-    sun.append(reys)
+for i in las:
+    reys=klj(j)
+    mnb.append(reys)
 kilo=sorted(mnb)
 kilo.reverse()
-for k in kilo:
-    print(int(k,2))
+for j in kilo:
+    print(int(j,2))
