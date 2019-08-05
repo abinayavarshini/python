a79185ebadb56f50978d9d3d8cfb9e735aafbbab
abinayavarshini <52314399+abinayavarshini@users.noreply.github.com>
2019-08-05 15:10:51 +0530

    Update 24pro.py

--- 24pro.py
+++ 24pro.py
@@ -2,11 +2,11 @@ bvx=int(input())
 lkh=2**bvx
 tr=[]
 for i in range(0,lkh):
-    jh=bin(i)[2:].trfill(bvx)
-    if(len(lt)<len(bin(2**bvx-1)[2:])):
-        tr.append([jh.count("1"),jh])
+    jb=bin(i)[2:].trfill(bvx)
+    if(len(jb)<len(bin(2**bvx-1)[2:])):
+        tr.append([jb.count("1"),jb])
     else:
-        tr.append([jh.count("1"),jh])
+        tr.append([jb.count("1"),jb])
 tr.sort()
 for i in range(len(tr)):
     print(tr[i][1]) 
