7b663d346de9cdeb2a856ff1be2116395a675593
abinayavarshini <52314399+abinayavarshini@users.noreply.github.com>
2019-08-05 14:01:38 +0530

    Update 18pro.py

--- 18pro.py
+++ 18pro.py
@@ -1,16 +1,16 @@
-abi,jon=map(int,input().split())
-hjg=[]
-plm=0
-for m in range(abi):
-    hjg.append(list(map(int,input().split())))   
-for m in range(abi):
-    for n in range(jon-1):
-        for o in range(n+1,jon+1):
-            if hjg[m][n:o]==[1]*len(hjg[m][n:o]):
-                 if all(hjg[p+m][n:o]==[1]*len(hjg[p+m][n:o]) for p in range(len hjg[m][n:o])-1)):
-                     if len(hjg[m][n:o])>plm:
-                        plm=len(hjg[m][n:o])
-if len(hjg)==1 and len(hjg[0])==1 and hjg[0][0]==1:
+abi,sei=map(int,input().split())
+srs=[]
+tqw=0
+for i in range(abi):
+    srs.append(list(map(int,input().split())))   
+for i in range(abi):
+    for j in range(sei-1):
+        for k in range(j+1,sei+1):
+            if srs[i][j:k]==[1]*len(srs[i][j:k]):
+                 if all(srs[p+i][j:k]==[1]*len(srs[p+i][j:k]) for p in range(len(srs[i][j:k])-1)):
+                     if len(srs[i][j:k])>tqw:
+                        tqw=len(srs[i][j:k])
+if len(srs)==1 and len(srs[0])==1 and srs[0][0]==1:
     print(1)
-for m in range(plm):
-    print(*[1]*plm)
+for i in range(tqw):
+    print(*[1]*tqw)
