afbb752f410b504747ae0ed4c7dfd23d27bfead6
abinayavarshini <52314399+abinayavarshini@users.noreply.github.com>
2019-08-05 14:41:59 +0530

    Update 4pro.py

--- 4pro.py
+++ 4pro.py
@@ -1,11 +1,11 @@
 abi,dan=map(str,input().split())
 jaw=0
 if len(abi)>len(dan):
-  abi,dan=dan,maht
-n=0
-while n<len(abi):
-  jaw+=(ord(dan[n])-ord(abi[n]))
-  n+=1
-for n in range(n,len(dan)):
-  jaw+=ord(dan[n])-ord('a')+1
+  abi,dan=dan,vhk
+i=0
+while i<len(abi):
+  jaw+=(ord(dan[i])-ord(abi[i]))
+  i+=1
+for i in range(i,len(dan)):
+  jaw+=ord(dan[i])-ord('a')+1
 print(jaw)
